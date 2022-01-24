489db9a251e70153ee9fbe6e09c3261d398a7f27
Onurcan Sever <onsever@icloud.com>
2022-01-24 17:59:04 -0500

    Initial Commit

---
+++ Lab2_OnurcanSever_C0830345_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2_OnurcanSever_C0830345_iOS
+//
+//  Created by Onurcan Sever on 2022-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
